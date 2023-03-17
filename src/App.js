3bf58b17d537b4c799fa8cd54d607a43cc377b23
JaynaMukesh <jaynamukee@gmail.com>
2023-03-17 15:57:13 +0530

    sparkss

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <div>
           <div className='mx-8 flex pb-48 justify-center flex-col h-[100%]'>
             <h1 className="text-[30px] font-bold text-[#18113D]">RewordMe - Sentence Rephraser AI</h1>
-            <span className='text-[15px] pt-4 font-bold text-[#4F4F4F]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam laoreet mi eros, eget lacinia massa pulvinar in. Sed luctus est sit amet volutpat dictum.</span>
+            <span className='text-[15px] pt-0 font-bold text-[#4F4F4F]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam laoreet mi eros, eget lacinia massa pulvinar in. Sed luctus est sit amet volutpat dictum.</span>
             <div>
               <Button className='mt-6' color={"secondary"} shadow>TRY IT OUT</Button>
             </div>
