c05c9e0f2989dde38210761ee9c5c37936160b83
Webxspark <webxspark@gmail.com>
2023-04-01 10:33:22 +0530

    API Security patch

--- science/rephrase.php
+++ science/rephrase.php
@@ -4,6 +4,7 @@ include './auth.php';
 $Auth = new Auth;
 $data = [];
 $length = 8;
+$fetchResp = true;
 //calculate the execution time
 $startTime = microtime(true);
 if ($_REQUEST) {
@@ -14,60 +15,76 @@ if ($_REQUEST) {
             if ($AuthCheck) {
                 $ValidateKeyCredits = $Auth->ValidateKeyCredits($conn, $APIKEY);
                 if ($ValidateKeyCredits) {
-                    $rephrase = htmlspecialchars($_REQUEST['sentence']);
-                    if(isset($_REQUEST['length'])){
-                        $length = (int)htmlspecialchars($_REQUEST['length']);
-                    }
-                    $curl = curl_init();
-
-                    curl_setopt_array($curl, array(
-                        CURLOPT_URL => "https://apis.webxspark.com/v2.0/text/rephrase",
-                        CURLOPT_RETURNTRANSFER => true,
-                        CURLOPT_ENCODING => "",
-                        CURLOPT_MAXREDIRS => 10,
-                        CURLOPT_TIMEOUT => 30,
-                        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                        CURLOPT_CUSTOMREQUEST => "POST",
-                        CURLOPT_POSTFIELDS => "s=$rephrase&size=$length",
-                    ));
-
-                    $response = curl_exec($curl);
-                    $response = json_decode($response, true);
-                    $sentences = [];
-                    foreach ($response['sentences'] as $key => $val) {
-                        unset($response['sentences'][$key]['Frequency']);
-                        unset($response['sentences'][$key]['Id']);
-                        unset($response['sentences'][$key]['Classification']);
-                        $sentences[$key] = $response['sentences'][$key]['Sentence'];
-                    }
-                    $response['sentences'] = $sentences;
-                    $err = curl_error($curl);
-
-                    curl_close($curl);
-
-                    if ($err) {
-                        $data['error'] = "EndPointReqError #:" . $err;
-                    } else {
-                        $data['response'] = $response;
+                    if($APIKEY === "com.beta.reword-me.webxspark.app"){
+                        //check if the request is from same domain for api security
+                        $domain = $_SERVER['HTTP_ORIGIN'];
+                        if($domain !== "https://rewordme.webxspark.com"){
+                            $data['error'] = "Access forbidden! Invalid domain!";
+                            $fetchResp = false;
+                        }
+                        //check if the request if from fetch api request of ajax request
+                        if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && ($_SERVER['HTTP_X_REQUESTED_WITH'] !== 'XMLHttpRequest')) {
+                            $data['error'] = "Access forbidden!";
+                            $fetchResp = false;
+                        }
                     }
 
-                    //update the credits
-                    $stmt = $conn->prepare("UPDATE auth SET credits = credits - 1 WHERE auth_key = ?");
-                    $stmt->bind_param('s', $APIKEY);
-                    $stmt->execute();
+                    if ($fetchResp) {
+                        $rephrase = htmlspecialchars($_REQUEST['sentence']);
+                        if (isset($_REQUEST['length'])) {
+                            $length = (int)htmlspecialchars($_REQUEST['length']);
+                        }
+                        $curl = curl_init();
 
-                    //insert analytics details
-                    $userName  = $Auth->fetchAPIKEYData($conn, $APIKEY)['username'];
-                    $ip = $Auth::fetch_ip();
-                    $endTime = microtime(true);
-                    $reqRespTime = ($endTime - $startTime);
+                        curl_setopt_array($curl, array(
+                            CURLOPT_URL => "https://apis.webxspark.com/v2.0/text/rephrase",
+                            CURLOPT_RETURNTRANSFER => true,
+                            CURLOPT_ENCODING => "",
+                            CURLOPT_MAXREDIRS => 10,
+                            CURLOPT_TIMEOUT => 30,
+                            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                            CURLOPT_CUSTOMREQUEST => "POST",
+                            CURLOPT_POSTFIELDS => "s=$rephrase&size=$length",
+                        ));
 
-                    $stmt = $conn->prepare("INSERT INTO requests(username,sentence,max_resp_time,ip) VALUES (?,?,?,?)");
-                    $stmt->bind_param('ssss', $userName, $rephrase, $reqRespTime, $ip);
-                    $stmt->execute();
+                        $response = curl_exec($curl);
+                        $response = json_decode($response, true);
+                        $sentences = [];
+                        foreach ($response['sentences'] as $key => $val) {
+                            unset($response['sentences'][$key]['Frequency']);
+                            unset($response['sentences'][$key]['Id']);
+                            unset($response['sentences'][$key]['Classification']);
+                            $sentences[$key] = $response['sentences'][$key]['Sentence'];
+                        }
+                        $response['sentences'] = $sentences;
+                        $err = curl_error($curl);
 
-                    $stmt = $conn->prepare("UPDATE analytics SET requests = requests + 1 WHERE id = 1");
-                    $stmt->execute();
+                        curl_close($curl);
+
+                        if ($err) {
+                            $data['error'] = "EndPointReqError #:" . $err;
+                        } else {
+                            $data['response'] = $response;
+                        }
+
+                        //update the credits
+                        $stmt = $conn->prepare("UPDATE auth SET credits = credits - 1 WHERE auth_key = ?");
+                        $stmt->bind_param('s', $APIKEY);
+                        $stmt->execute();
+
+                        //insert analytics details
+                        $userName  = $Auth->fetchAPIKEYData($conn, $APIKEY)['username'];
+                        $ip = $Auth::fetch_ip();
+                        $endTime = microtime(true);
+                        $reqRespTime = ($endTime - $startTime);
+
+                        $stmt = $conn->prepare("INSERT INTO requests(username,sentence,max_resp_time,ip) VALUES (?,?,?,?)");
+                        $stmt->bind_param('ssss', $userName, $rephrase, $reqRespTime, $ip);
+                        $stmt->execute();
+
+                        $stmt = $conn->prepare("UPDATE analytics SET requests = requests + 1 WHERE id = 1");
+                        $stmt->execute();
+                    }
                 } else {
                     $data['error'] = "No credits left! Your free trial credits are over!";
                 }
