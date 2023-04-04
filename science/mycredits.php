4e5339355787d0ae8493bc4c04be7d6632b6c264
Jayna Mukesh <115871133+JaynaMukesh@users.noreply.github.com>
2023-04-04 08:42:01 +0000

    Update: completed mycredits.php

--- science/mycredits.php
+++ science/mycredits.php
@@ -8,16 +8,14 @@ if(isset($_REQUEST['key'])){
     $APIKEY = htmlspecialchars($_REQUEST['key']);
     $AuthCheck = $Auth->CheckAPIKEY($conn, $APIKEY);
     if($AuthCheck){
-        $stmt = $conn->prepare("SELECT * FROM auth_key WHERE key=? LIMIT 1");
+        $stmt = $conn->prepare("SELECT auth_key,credits FROM auth_key WHERE key=? LIMIT 1");
         $stmt->bind_param('s', $APIKEY);
         $stmt->execute();
         $result = $stmt->get_result();
         $user = mysqli_fetch_assoc($result);
         $data = [
             "context" => [
-                "content" => [
-                    
-                ]
+                "content" => $user
             ]
         ];
     } else {
