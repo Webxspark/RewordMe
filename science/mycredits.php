dc529651e2210b546cb7dd6e55243194a431fc75
Jayna Mukesh <115871133+JaynaMukesh@users.noreply.github.com>
2023-04-04 08:47:56 +0000

    Update: mycred sql query fix

--- science/mycredits.php
+++ science/mycredits.php
@@ -8,7 +8,7 @@ if(isset($_REQUEST['key'])){
     $APIKEY = htmlspecialchars($_REQUEST['key']);
     $AuthCheck = $Auth->CheckAPIKEY($conn, $APIKEY);
     if($AuthCheck){
-        $stmt = $conn->prepare("SELECT auth_key,credits FROM auth_key WHERE key=? LIMIT 1");
+        $stmt = $conn->prepare("SELECT auth_key,credits FROM auth WHERE auth_key=? LIMIT 1");
         $stmt->bind_param('s', $APIKEY);
         $stmt->execute();
         $result = $stmt->get_result();
