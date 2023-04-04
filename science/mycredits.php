7e40664788952b2a6508378260d2d9bc1e3c1fe0
Jayna Mukesh <115871133+JaynaMukesh@users.noreply.github.com>
2023-04-04 08:59:32 +0000

    update: minor fixes

--- science/mycredits.php
+++ science/mycredits.php
@@ -8,7 +8,8 @@ if(isset($_REQUEST['key'])){
     $APIKEY = htmlspecialchars($_REQUEST['key']);
     $AuthCheck = $Auth->CheckAPIKEY($conn, $APIKEY);
     if($AuthCheck){
-        $stmt = $conn->prepare("SELECT auth_key,credits FROM auth WHERE auth_key=? LIMIT 1");
+        if($APIKEY !== "com.beta.reword-me.webxspark.app"){
+          $stmt = $conn->prepare("SELECT auth_key,credits FROM auth WHERE auth_key=? LIMIT 1");
         $stmt->bind_param('s', $APIKEY);
         $stmt->execute();
         $result = $stmt->get_result();
@@ -17,7 +18,11 @@ if(isset($_REQUEST['key'])){
             "context" => [
                 "content" => $user
             ]
-        ];
+        ];  
+        } else {
+            $data['error'] = "Access denied!";
+        }
+        
     } else {
         $data['error'] = "Invalid API Key!";
     }
